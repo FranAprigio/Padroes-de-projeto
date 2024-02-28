c23ffe6265ad6c372316c1b1f59665dbb9c1ee39
Franciane Aprigio <francianefad@outlook.com>
2024-02-27 21:00:15 -0300

    First Commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.example.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testar() {
+
+    assertEquals("10;20", Main.teste());
+
+    }
+}
